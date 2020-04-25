ff36b24a44fed6977da5ec67c6f99622741cb149
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-04-24 22:09:46 -0400

    FIX: Forgot to commit some files

---
+++ build/precache-manifest.e9e9b18d73e5afadfe4123f393b9007e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49952e8323d4a5b12a1854770779b274",
+    "url": "/static/media/twitter.49952e83.png"
+  },
+  {
+    "revision": "bdcfffc2913eb9e61a79a41744564e42",
+    "url": "/static/media/favicon-64x64w.bdcfffc2.png"
+  },
+  {
+    "revision": "f0c8fe1c2940ba5cafce740e520cb46f",
+    "url": "/static/media/ChordGenFaviconw.f0c8fe1c.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d71532842037a693129",
+    "url": "/static/js/main.a512b89e.chunk.js"
+  },
+  {
+    "revision": "09a84626e0da8fb3c150",
+    "url": "/static/js/2.539a7e4c.chunk.js"
+  },
+  {
+    "revision": "0ccb76cdd184a129843f4be0b10cb6ae",
+    "url": "/index.html"
+  }
+];
